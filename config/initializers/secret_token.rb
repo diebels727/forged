c400c88d4f261c2220d7ab2cf7e28e55a08e40c2
diebels727 <diebelsalternative@hotmail.com>
2013-09-29 13:27:01 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Forged::Application.config.secret_token = '3c34feb14a515916597a3a2fc61c275ee510fb1922c0fbaa92c3936d30deab6c9e5f616ce4fd1d981be6530729d1bbcb00ff52ebeb50ced0a2aecf5b4c113710'
